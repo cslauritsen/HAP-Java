55896c193eed9b99f86fc8b5893ea02a4a60a0a8
Chad Lauritsen <csl4jc@gmail.com>
2016-10-19 06:59:22 -0400

    new deps, and trying to add persistent auth

--- src/main/java/com/beowulfe/hap/sample/MockAuthInfo.java
+++ src/main/java/com/beowulfe/hap/sample/MockAuthInfo.java
@@ -7,6 +7,14 @@ import java.util.concurrent.ConcurrentMap;
 
 import com.beowulfe.hap.HomekitAuthInfo;
 import com.beowulfe.hap.HomekitServer;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /**
  * This is a simple implementation that should never be used in actual production. The mac, salt, and privateKey
@@ -15,7 +23,7 @@ import com.beowulfe.hap.HomekitServer;
  *
  * @author Andy Lintner
  */
-public class MockAuthInfo implements HomekitAuthInfo {
+public class MockAuthInfo implements HomekitAuthInfo, java.io.Serializable {
 	
 	private static final String PIN = "031-45-154";
 	
@@ -24,13 +32,51 @@ public class MockAuthInfo implements HomekitAuthInfo {
 	private final byte[] privateKey;
 	private final ConcurrentMap<String, byte[]> userKeyMap = new ConcurrentHashMap<>();
 	
-	public MockAuthInfo() throws InvalidAlgorithmParameterException {
-		mac = HomekitServer.generateMac();
-		salt = HomekitServer.generateSalt();
-		privateKey = HomekitServer.generateKey();
-		System.out.println("Auth info is generated each time the sample application is started. Pairings are not persisted.");
-		System.out.println("The PIN for pairing is "+PIN);
+	public MockAuthInfo() throws IOException, InvalidAlgorithmParameterException {
+            StringBuilder sb = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new FileReader("auth.mac"))) {
+                for (String line = br.readLine(); line != null; line = br.readLine()) {
+                    sb.append(line);
+                }
+                System.out.println("Loaded auth.mac");
+            }
+            catch (Exception e) {
+                sb.setLength(0);
+		sb.append(HomekitServer.generateMac());
+            }
+            mac = sb.toString();
+            try (FileWriter fw = new FileWriter("auth.mac")) {
+                fw.write(mac);
+            }
+            
+            BigInteger tmpBigInt = null;
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("auth.salt"))) {
+                tmpBigInt = (BigInteger) ois.readObject();
+                System.out.println("Loaded auth.salt");
+            }
+            catch (Exception e) {
+                tmpBigInt = HomekitServer.generateSalt();
+            }
+            salt = tmpBigInt;
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("auth.salt"))) {
+                oos.writeObject(salt);
+            }
+            
+            byte[] tmpKey = null;
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("auth.key"))) {
+                tmpKey = (byte[]) ois.readObject();
+                System.out.println("Loaded auth.key");
+            }
+            catch (Exception e) {
+                tmpKey = HomekitServer.generateKey();
+            }
+            privateKey = tmpKey;
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("auth.key"))) {
+                oos.writeObject(privateKey);
+            }
+            System.out.println("The PIN for pairing is "+PIN);
 	}
+
 
 	@Override
 	public String getPin() {
